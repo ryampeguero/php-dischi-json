e549c73a350257f4ef34e7c4791b6c662e1ebbd7
ryampeguero <ryampeguero@gmail.com>
2024-05-23 13:12:29 +0200

    feat: server.php now is an API

--- server.php
+++ server.php
@@ -1,1 +1,10 @@
 <?php
+$disk_list = file_get_contents("dischi.json");
+$disk_decoded = json_decode($disk_list, true);
+
+header("Content-type: application/json");
+
+
+$results = json_encode($disk_decoded);
+//Invio risposta
+echo $results;
